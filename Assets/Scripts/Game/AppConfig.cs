822309e75304755359844efde173cbe73cb3e962
ClaineLe <7941324@qq.com>
2018-09-03 19:03:36 +0800

    增加应用程序设置类

---
+++ Assets/Scripts/Game/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Framework
+{
+    namespace Game
+    {
+        public class AppConfig
+        {
+            public const int Change = 1;
+            public const string AppVersion = "1.0.0";
+            public const string ResVersion = "2.0.0";
+            public const string LuaVersion = "3.0.0";
+
+            public const string FileServerIP = "192.168.0.0";
+        }
+    }
+}
