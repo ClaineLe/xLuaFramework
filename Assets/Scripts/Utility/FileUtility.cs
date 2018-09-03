dbcb9993911dd69407120b4890a6093a19561cfe
ClaineLe <7941324@qq.com>
2018-09-03 19:04:05 +0800

    文件工具

---
+++ Assets/Scripts/Utility/FileUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    namespace Util
+    {
+        public class FileUtility
+        {
+            /// <summary>
+            /// 复制文件到目标路径
+            /// </summary>
+            /// <param name="scrPath">源目录路径</param>
+            /// <param name="dstPath">目标目录路径</param>
+            static public void DirectoryCopyTo(string scrPath, string dstPath){
+            }
+        }
+    }
+}
