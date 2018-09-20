33c252f2800a8bfbbff7f679efce0fa7a564e3ff
ClaineLe <7941324@qq.com>
2018-09-20 17:01:14 +0800

    目录工具类

--- Assets/Scripts/Utility/DirectoryUtility.cs
+++ Assets/Scripts/Utility/DirectoryUtility.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 namespace Framework
 {
     namespace Util
@@ -14,6 +14,24 @@ namespace Framework
             /// <param name="scrPath">源文件夹路径</param>
             /// <param name="dstPath">目标文件夹路径</param>
             static public void DirectoryCopyTo(string scrPath, string dstPath){
+            }
+
+            static public bool Exists(string path){
+                return Directory.Exists(path);
+            }
+
+            static public void Clear(string path)
+            {
+                Delete(path);
+                Directory.CreateDirectory(path);
+            }
+
+            static public void Delete(string path)
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
             }
         }
     }
