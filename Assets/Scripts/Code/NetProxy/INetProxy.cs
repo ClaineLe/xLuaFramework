2d6930c7366c942b150db6933f0af752569acad9
ClaineLe <7941324@qq.com>
2018-09-21 18:10:20 +0800

    增加网络消息代理接口

---
+++ Assets/Scripts/Code/NetProxy/INetProxy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	namespace Code.NetProxy
+	{
+		public delegate string NetMessageListenDelegate (byte[] netData);
+
+		public interface INetProxy
+		{
+			void RegiestNetMsg(ushort cmd, NetMessageListenDelegate netMsgHandle);
+			void UnRegiestNetMsg(ushort cmd);
+			void Release();
+		}
+	}
+}
---
+++ Assets/Scripts/Code/NetProxy/NetProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	namespace Code.NetProxy
+	{
+		public class NetProxy : INetProxy
+		{
+			public void RegiestNetMsg (ushort cmd, NetMessageListenDelegate netMsgHandle)
+			{
+				throw new System.NotImplementedException ();
+			}
+			public void UnRegiestNetMsg (ushort cmd)
+			{
+				throw new System.NotImplementedException ();
+			}
+			public void Release ()
+			{
+				throw new System.NotImplementedException ();
+			}
+		}
+	}
+}
