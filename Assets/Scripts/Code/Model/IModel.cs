b8fde9525f2704c179205908d5eda4290b306c6f
ClaineLe <7941324@qq.com>
2018-09-21 18:10:05 +0800

    增加模块接口

---
+++ Assets/Scripts/Code/Model/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	namespace Code.Model
+	{
+		public interface IModel
+		{
+			int ModelId{ get; }
+			string ModelName{ get; }
+			void Release();
+		}
+	}
+}
---
+++ Assets/Scripts/Code/Model/Model.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	namespace Code.Model
+	{
+		public class Model : IModel
+		{
+			private int m_Id;
+			public int ModelId {
+				get {
+					return m_Id;
+				}
+			}
+
+			private string m_Name;
+			public string ModelName {
+				get {
+					return m_Name;
+				}
+			}
+
+			public Model (int modelId, string modelName)
+			{
+				this.m_Id = modelId;
+				this.m_Name = modelName;
+			}
+
+			public void RegiestNetProxy(){
+				
+			}
+
+			public void Release ()
+			{
+			}
+		}
+	}
+}
