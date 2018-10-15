a2293daf829ee27a87f700576084fe4a6a51c12a
ClaineLe <7941324@qq.com>
2018-10-16 01:21:23 +0800

    增加，定时管理器

--- Assets/Scripts/Code/Manager/TimerManager/TimerManager.cs
+++ Assets/Scripts/Code/Manager/TimerManager/TimerManager.cs
@@ -1,33 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Framework
 {
-    namespace Code.Manager
-    {
-        public partial class ManagerName
-        {
-            public const string Timer = "TimerManager";
-        }
-        public class TimerManager : BaseManager<TimerManager>, IManager
-        {
-            public void Init()
-            {
-            }
+	namespace Code.Manager
+	{
+		public partial class ManagerName
+		{
+			public const string Timer = "TimerManager";
+		}
+		public class TimerManager : BaseManager<TimerManager>, IManager
+		{
+			private List<Timer> _timerList;
+			public int TimerCnt{
+				get{ 
+					if (_timerList == null)
+						return 0;
+					return _timerList.Count;
+				}
+			}
+			public void Init ()
+			{
+				this._timerList = new List<Timer> ();
+			}
 
-            public void Release()
-            {
-            }
+			public void Release ()
+			{
+			}
 
-            public void Tick()
-            {
-            }
+			public void Tick ()
+			{
+				if (_timerList.Count > 0) {
+					for (int i = 0; i < _timerList.Count;i++ ) {
+						_timerList [i].Tick ();
+					}
+				}
+			}
 
-            public void Test()
-            {
-                Debug.Log("CSharp - Test");
-            }
-        }
-    }
+			public Timer AddTimer (float interval, int times, Action<float> onTick, Action onStart = null, Action<int> onPreAction = null, Action onFinish = null, bool awakeStart = true, bool autoRemove = true)
+			{
+				Timer timer = new Timer (interval,times,onTick,onStart,onPreAction,onFinish,awakeStart,autoRemove);
+				_timerList.Add (timer);
+				return timer;
+			}
+
+			public void RemoveTimer(Timer timer){
+				_timerList.Remove (timer);
+			}
+		}
+	}
 }
