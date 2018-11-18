f197f5c57066a128bce244add72f7de893089767
ClaineLe <7941324@qq.com>
2018-11-19 00:29:32 +0800

    修改python导表工具，支持Excel换行

--- ExcelToCSV/run.py
+++ ExcelToCSV/run.py
@@ -1,23 +1,18 @@
 
 import xlrd
-import csv
-import codecs
 import sys
 import os
 import shutil
 
 reload(sys)
-sys.setdefaultencoding( "utf-8" )
+sys.setdefaultencoding("utf-8")
 
 CUR_DIR_FULL_PATH = os.path.dirname(os.path.realpath(__file__))
 
-PROJECT_CSV_FULL_PATH = CUR_DIR_FULL_PATH + "/../Assets/AppAssets/Xls"
-
 EXCEL_DIR_PATH = CUR_DIR_FULL_PATH + "/excel"
-EXCEL_SUFFIX_NAME = ".xlsx"
-
 CSV_DIR_PATH = CUR_DIR_FULL_PATH + "/csv"
-CSV_SUFFIX_NAME = ".csv"
+PROJECT_CSV_FULL_PATH = CUR_DIR_FULL_PATH + "/../Assets/AppAssets/#Xls"
+#row_value = row_value.replace('\n','<br/>')
 
 def sheet_to_csv(xlsx_name,sheet):
 	try:
@@ -25,13 +20,22 @@ def sheet_to_csv(xlsx_name,sheet):
 			return
 		csv_file_name = sheet.cell_value(1, 1)
 		if csv_file_name.strip():
-			csv_full_path = CSV_DIR_PATH + "/" + csv_file_name + CSV_SUFFIX_NAME
-			with codecs.open(csv_full_path, 'w', encoding='utf-8') as f:
-				write = csv.writer(f)
-				for row_num in range(sheet.nrows):
-					row_value = sheet.row_values(row_num)
-					write.writerow(row_value)
-				print("Export Success: => " + sheet.name + "(" + csv_file_name + ")")
+			csv_full_path = CSV_DIR_PATH + "/" + csv_file_name + ".csv"
+			file = open(csv_full_path, 'w')
+			row_data = ""
+			for row_num in range(sheet.nrows):
+				for col_num in range(sheet.ncols):
+					tmp_cell_value = sheet.cell_value(row_num, col_num)
+					tmp_cell_type = sheet.cell_type(row_num, col_num)
+					if tmp_cell_type == 1:
+						tmp_cell_value = tmp_cell_value.replace("\n","</br>")
+					elif tmp_cell_type == 2:
+						tmp_cell_value = int(tmp_cell_value)
+					tmp_cell_value = str(tmp_cell_value).strip()
+					row_data = row_data + "{0},".format(tmp_cell_value)
+				row_data = row_data[:-1] + "\n"
+			file.write(row_data[:-1])
+			print("Export Success: => " + sheet.name + "(" + csv_file_name + ")")
 	except BaseException,err:
 		print("---------------------------------------------------")
 		print("[Export Excel To Csv Error]")
@@ -54,7 +58,7 @@ def excel_to_sheet(xlsx_name):
 def collect_all_excel():
 	for root, dirs, files in os.walk( EXCEL_DIR_PATH):  
 		for file in files:  
-			if os.path.splitext(file)[1] == EXCEL_SUFFIX_NAME:  
+			if os.path.splitext(file)[1] == ".xlsx":  
 				if not file.startswith("~$"):
 					excel_to_sheet(file)
 
@@ -66,18 +70,18 @@ def delete_all_csv(dest_path):
             del_file(c_path)
         else:
             os.remove(c_path)
-    print("clear all csv success!")
+    print("clear all csv success! => " + dest_path)
 
 def copy_csv_to_project():
 	for root, dirs, files in os.walk( CSV_DIR_PATH):  
 		for file in files:  
-			if os.path.splitext(file)[1] == CSV_SUFFIX_NAME:  
+			if os.path.splitext(file)[1] == ".csv":
 				scr_full_path = CSV_DIR_PATH + "/" + file
 				dst_full_path = PROJECT_CSV_FULL_PATH + "/" + file
 				shutil.copy(scr_full_path,dst_full_path)
+	print("copy all csv to project success!")
 
 if __name__ == '__main__':
-	print(PROJECT_CSV_FULL_PATH)
 	delete_all_csv(CSV_DIR_PATH)
 	collect_all_excel()
 	delete_all_csv(PROJECT_CSV_FULL_PATH)
