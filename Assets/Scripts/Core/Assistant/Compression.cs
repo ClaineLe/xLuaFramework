e1b8b09fb06a51ad156f0faac651bd9a0251a45d
ClaineLe <7941324@qq.com>
2018-11-23 23:36:46 +0800

    移除冗余接口

--- Assets/Scripts/Core/Assistant/Compression.cs
+++ Assets/Scripts/Core/Assistant/Compression.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.IO.Compression;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class CompressionHelper
