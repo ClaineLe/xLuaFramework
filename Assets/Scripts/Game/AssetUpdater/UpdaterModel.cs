52004740bf19fa67456909e839911698ca430b1e
ClaineLe <7941324@qq.com>
2018-11-24 20:15:32 +0800

    初步完成热更逻辑代码

--- Assets/Scripts/Game/AssetUpdater/UpdaterModel.cs
+++ Assets/Scripts/Game/AssetUpdater/UpdaterModel.cs
@@ -1,8 +1,11 @@
-﻿using Framework.Core.Singleton;
+﻿using Framework.Core.Assistant;
+using Framework.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Framework
 {
@@ -10,8 +13,19 @@ namespace Framework
     {
         public class UpdaterModel : SingletonMono<UpdaterModel>
         {
+            public int updateState = 0;
+            private static string[] NOTICES = new[] {
+                "获取补丁包列表",
+                "下载版本:{0},补丁包大小:{1}",
+                "解压补丁包...",
+                "更新版本:{0}完成",
+                "即将进入游戏"
+            };
+
+
             private const string APP_INFO_FILENAME = AppConst.FileServerAddress + AppConst.AppInfoFileName;
 
+            private List<PackerInfo> _waitDownLoadPackerInfoList;
             private UpdaterPresender _presender;
             private System.Action _onUpdateAssetFinish;
 
@@ -23,28 +37,74 @@ namespace Framework
             public void Lanucher(System.Action onUpdateAssetFinish)
             {
                 this._onUpdateAssetFinish = onUpdateAssetFinish;
-                StartCoroutine(LoadServerAppInfo(str=> {
-                    Debug.Log(str);
-                    this.Complete();
-                }));
+                updateState = 0;
+                StartCoroutine(DownLoadPackerInfoList());
             }
 
-            private IEnumerator LoadServerAppInfo(System.Action<string> appInfo_callback) {
-                using (UnityWebRequest www = UnityWebRequest.Get(APP_INFO_FILENAME))
+            private IEnumerator DownLoadPackerInfoList()
+            {
+                _presender.SetNotice(NOTICES[updateState]);
+                yield return NetWorkUtility.GetHttpContent(APP_INFO_FILENAME, (isError, content) =>
                 {
-                    yield return www.SendWebRequest();
-                    string str = string.Empty;
-                    if (www.isNetworkError || www.isHttpError)
+                    Debug.Log(isError + ":" + content);
+                    if (!isError)
                     {
-                        str = www.error;
+                        List<PackerInfo> allPackerInfoList = JsonConvert.DeserializeObject<List<PackerInfo>>(content);
+                        _waitDownLoadPackerInfoList = CollectNeedUpdatePacker(allPackerInfoList);
                     }
-                    else
-                    {
-                        str = www.downloadHandler.text;
-                    }
-                    if (appInfo_callback != null)
-                        appInfo_callback(str);
+                });
+                yield return new WaitUntil(() => _waitDownLoadPackerInfoList != null);
+                updateState = 1;
+            }
+
+            private List<PackerInfo> CollectNeedUpdatePacker(List<PackerInfo> allPackerList)
+            {
+                return null;
+            }
+
+            private IEnumerator DownLoadPackerList()
+            {
+                while (_waitDownLoadPackerInfoList.Count > 0)
+                {
+                    yield return DownLoadPackerBase(_waitDownLoadPackerInfoList[0]);
+                    _waitDownLoadPackerInfoList.RemoveAt(0);
                 }
+                yield return new WaitUntil(()=> _waitDownLoadPackerInfoList.Count == 0);
+                updateState = 4;
+                _presender.SetNotice(NOTICES[updateState]);
+                yield return new WaitForSeconds(0.5f);
+                this.Complete();
+            }
+
+            private IEnumerator DownLoadPackerBase(PackerInfo packerInfo)
+            {
+                updateState = 1;
+                string notice = string.Format(NOTICES[updateState], packerInfo.packerName, packerInfo.packerSize);
+                _presender.SetNotice(notice);
+
+                string url = AppConst.FileServerAddress + packerInfo.packerName;
+                string saveFullPath = PathConst.LocalAssetCacheDirPath + packerInfo.packerName;
+
+                UnityWebRequestAsyncOperation operation = NetWorkUtility.DownLoadZip(url, saveFullPath);
+                DownloadHandleContinue handle = operation.webRequest.downloadHandler as DownloadHandleContinue;
+                while (!operation.isDone)
+                {
+                    yield return null;
+                    _presender.SetProgress(operation.progress);
+                }
+                yield return new WaitUntil(() => operation.isDone);
+                CompressionHelper.DeCompress(saveFullPath, "");
+                UpdateLocalAssetVersion(packerInfo.toVersion);
+                yield return new WaitForSeconds(0.2f);
+            }
+
+            private void UpdateLocalAssetVersion(string dstVersion)
+            {
+                updateState = 2;
+                _presender.SetNotice(NOTICES[updateState]);
+                /*
+                更新本地版本信息逻辑
+                */
             }
 
             private void Complete()
@@ -56,7 +116,6 @@ namespace Framework
                 this._presender.Release();
                 base.Release();
             }
-
         }
     }
 }
