6fe3f7e58629444d5a35fa720139d33ef15d9e3f
ClaineLe <7941324@qq.com>
2018-11-24 20:13:35 +0800

    增加断点续传下载接口，获取Http网络信息接口

---
+++ Assets/Scripts/Utility/NetWorkUtility/DownloadHandleContinue.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Framework.Util;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Framework.Core
+{
+    namespace Assistant
+    {
+        public class DownloadHandleContinue : DownloadHandlerScript
+        {
+            private FileStream m_Stream;
+
+            public string m_IsError { get; protected set; }
+            private string _tmpExtension;
+            private string _saveFullPath;
+
+            private long _localSize;
+            private long _totalSize;
+            private string tmpFileFullPath{
+                get{
+                    return Path.ChangeExtension(_saveFullPath, _tmpExtension);
+                }
+            }
+
+            public long GetLocalSize()
+            {
+                return _localSize;
+            }
+
+            public DownloadHandleContinue(string saveFullPath, int preCacheSize = 200, string tmpExtension = ".temp"):base(new byte[1024 * preCacheSize]){
+                _saveFullPath = saveFullPath;
+                _tmpExtension = tmpExtension;
+
+                m_Stream = new FileStream(tmpFileFullPath, FileMode.Append, FileAccess.Write);    //文件流操作的是临时文件，结尾添加.temp扩展名
+                _localSize = m_Stream.Length;
+                m_Stream.Position = _localSize; 
+            }
+
+            protected override float GetProgress()
+            {
+                if (_localSize > 0 && _totalSize > 0)
+                    return (float)(_localSize / (double)_totalSize);
+                else
+                    return 0;
+            }
+            protected override void CompleteContent()
+            {
+                ReleaseStream();
+                if (File.Exists(tmpFileFullPath)){
+                    if (File.Exists(_saveFullPath))
+                        File.Delete(_saveFullPath);
+                    File.Move(tmpFileFullPath, _saveFullPath);
+                }else{
+                    m_IsError = "Found out DownLoadFile. SaveFullPath:" + tmpFileFullPath;
+                }
+            }
+
+            protected override void ReceiveContentLength(int contentLength)
+            {
+                this._totalSize = _localSize + contentLength;
+            }
+
+            protected override bool ReceiveData(byte[] data, int dataLength)
+            {
+                if (dataLength > 0)
+                {
+                    m_Stream.Write(data, 0, dataLength);
+                    _localSize += dataLength;
+                    return true;
+                }
+                return false;
+            }
+
+            public void ReleaseStream(){
+                if(m_Stream != null)
+                {
+                    m_Stream.Close();
+                    m_Stream.Dispose();
+                    m_Stream = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/NetWorkUtility/NetWorkUtility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Framework.Util;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Framework.Core
+{
+    namespace Assistant
+    {
+        public class NetWorkUtility
+        {
+            public static UnityWebRequestAsyncOperation DownLoadZip(string url,string savePath)
+            {
+                UnityWebRequest request = UnityWebRequest.Get(url);
+                DownloadHandleContinue handle = new DownloadHandleContinue(savePath);
+                request.disposeDownloadHandlerOnDispose = true;
+                request.SetRequestHeader("Range", "bytes=" + handle.GetLocalSize() + "-"); //断点续传设置读取文件数据流开始索引，成功会返回206
+                request.downloadHandler = handle;
+                return request.SendWebRequest();
+            }
+
+            public static IEnumerator GetHttpContent(string url, System.Action<bool, string> onComplete)
+            {
+                using (UnityWebRequest request = UnityWebRequest.Get(url))
+                {
+                    yield return request.SendWebRequest();
+                    bool isError = request.isNetworkError || request.isHttpError;
+                    string content = isError ? request.error : request.downloadHandler.text;
+                    onComplete?.Invoke(isError, content);
+                }
+            }
+        }
+    }
+}
