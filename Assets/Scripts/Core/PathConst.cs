09deacb4ff0f3ce922298173be66d13732594edd
ClaineLe <7941324@qq.com>
2018-11-25 17:51:23 +0800

    增加编辑器BundleRunTime目录

--- Assets/Scripts/Core/PathConst.cs
+++ Assets/Scripts/Core/PathConst.cs
@@ -3,8 +3,9 @@
 namespace Framework
 {
     namespace Game
-    { 
-        public class PathConst{
+    {
+        public class PathConst
+        {
             public const string ViewRoot_Path = "Prefabs/#View/ViewRoot.prefab";
             public const string ViewLayer_Path = "Prefabs/#View/Layer.prefab";
             public const string ViewAsset_Path = "Prefabs/#View/{0}.prefab";
@@ -13,24 +14,39 @@ namespace Framework
             public const string FORMAT_LUAROOT = "Lua/";
             public const string LUA_FRAMEWORK = "#core.Framework";
             public const string FORMAT_PRESENDER_NAME = "view.{0}.{1}Presender";
+            public const string BundleDirName = "AssetBundls";
+            public static string StreamAssetPath
+            {
+                get
+                {
+#if UNITY_EDITOR
+                    return Application.dataPath + "/../StreamAsset/";
+#else
+                    return Application.streamingAssetsPath + "/";
+#endif
+                }
+            }
 
-			public static string StreamAssetPathInAsset => Application.streamingAssetsPath + "/AssetBundles/";
+
 
             public static string LocalAssetCacheDirPath => "";
 #if UNITY_EDITOR
+            public static string CurChangePlatformRelativePath{
+                get{
+                    return string.Format("{0}_{1}/", AppConst.Change, AppConst.GetPlatformName());
+                }
+            }
+            public static string BuildBundleRootPath{
+                get{
+                    return Application.dataPath + "/../" + BundleDirName + "/";
+                }
+            }
             public const string ResRelativePath = "res/" + AppConst.ResVersion + "/";
             public const string XlsRelativePath = "xls/" + AppConst.XlsVersion + "/";
             public const string LuaRelativePath = "lua/" + AppConst.LuaVersion + "/";
 
             public const string ExportResDirPath = "AppAssets/";
 
-            public static string StreamAssetPath
-            {
-                get
-                {
-                    return Application.dataPath + "/../AssetBundles/" + AppConst.Change.ToString() + "/";
-                }
-            }
 
             public static string PlayerOutPutPath
             {
--- Assets/Scripts/Game/PathRoute.cs
+++ Assets/Scripts/Game/PathRoute.cs
@@ -7,27 +7,12 @@ namespace Framework
 {
     namespace Game
     {
-#if UNITY_EDITOR
-    #if BUNDLE_MODEL
-            public class PathRoute
-            {
-                public static string GetAssetBundleFullPath(string bundleName)
-                {
-                    if (bundleName.StartsWith("#xls"))
-                        return PathConst.StreamAssetPath + AppConst.GetPlatformName() + "/" + PathConst.XlsRelativePath + bundleName;
-                    else if (bundleName.StartsWith("lua"))
-                        return PathConst.StreamAssetPath + AppConst.GetPlatformName() + "/" + PathConst.LuaRelativePath + bundleName;
-                    else
-                        return PathConst.StreamAssetPath + AppConst.GetPlatformName() + "/" + PathConst.ResRelativePath + bundleName;
-                }
-            }
-    #endif
-#else
+#if !UNITY_EDITOR || BUNDLE_MODEL
         public class PathRoute
         {
             public static string GetAssetBundleFullPath(string bundleName)
-            {            
-                return PathConst.StreamAssetPathInAsset + "/" + bundleName;
+            {
+                return PathConst.StreamAssetPath + PathConst.BundleDirName + "/" + bundleName;
             }
         }
 #endif
